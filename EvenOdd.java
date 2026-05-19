52c7cd1e2a4f7040126393a6e7258bdfcf10e413
Ritika-23-2007 <ritika.agrwl.2007@gmail.com>
2026-05-19 08:13:39 +0530

    save changes to evenodd

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,15 +1,22 @@
- import java.util.Scanner;
-public class EvenOdd  {
+import java.util.Scanner;
+public class EvenOdd {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+
         System.out.print("Enter an integer: ");
         int number = scanner.nextInt();
 
+
+        String type ;
         if (number % 2 == 0) {
-            System.out.println(number + " is an even number.");
+            type = "even";
         } else {
-            System.out.println(number + " is an odd number.");
+            type = "odd";
         }
+        System.out.println(number + " is an " + type + " number.");
+
+        
 
         scanner.close();
     }
