7d61533a3def660329e33f2191052b87508a6d69
Ritika-23-2007 <ritika.agrwl.2007@gmail.com>
2026-05-19 16:31:30 +0530

    save quadrant.java

--- if_else/Quadrant.java
+++ if_else/Quadrant.java
@@ -8,21 +8,14 @@ public class Quadrant {
         System.out.print("Enter the y-coordinate: ");
         double y = sc.nextDouble();
 
-        if(x>0 && y>0) {
-            System.out.println("The point (" + x + ", " + y + ") is in the first quadrant.");
-        } else if(x<0 && y>0) {
-            System.out.println("The point (" + x + ", " + y + ") is in the second quadrant.");
-        } else if(x<0 && y<0) {
-            System.out.println("The point (" + x + ", " + y + ") is in the third quadrant.");
-        } else if(x>0 && y<0) {
-            System.out.println("The point (" + x + ", " + y + ") is in the fourth quadrant.");
-        } else if(x==0 && y!=0) {
-            System.out.println("The point (" + x + ", " + y + ") is on the y-axis.");
-        } else if(x!=0 && y==0) {
-            System.out.println("The point (" + x + ", " + y + ") is on the x-axis.");
-        } else {
-            System.out.println("The point (" + x + ", " + y + ") is at the origin.");
-        }
+        if(x>0 && y>0) {  System.out.println("The point (" + x + ", " + y + ") is in the first quadrant."); }  
+        else if(x<0 && y>0) {System.out.println("The point (" + x + ", " + y + ") is in the second quadrant."); }
+        else if(x<0 && y<0) {System.out.println("The point (" + x + ", " + y + ") is in the third quadrant."); }
+        else if(x>0 && y<0) {System.out.println("The point (" + x + ", " + y + ") is in the fourth quadrant."); }
+        else if(x==0 && y!=0) {System.out.println("The point (" + x + ", " + y + ") is on the y-axis.");  } 
+        else if(x!=0 && y==0) {System.out.println("The point (" + x + ", " + y + ") is on the x-axis."); }
+        else {System.out.println("The point (" + x + ", " + y + ") is at the origin."); }
+
         sc.close();
     }   
 }
