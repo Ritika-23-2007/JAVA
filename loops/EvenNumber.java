6aadce1d3c5dc0231bb8f15262c467ebb8ac8605
Ritika-23-2007 <ritika.agrwl.2007@gmail.com>
2026-05-21 20:39:18 +0530

    save todays work

--- loops/EvenNumber.java
+++ loops/EvenNumber.java
@@ -3,8 +3,8 @@ package loops;
 public class EvenNumber {
     public static void main(String[]args) {
         int a;
-        for (a=2; a<=100; a+=2) {
-           System.out.println(a);
+        for (a=2 ; a<101; a+=2) {
+           System.out.println(a );
         }
     }
 }
