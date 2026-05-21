f21069600427d0c1f4df02ab544fba3503e9f849
Ritika-23-2007 <ritika.agrwl.2007@gmail.com>
2026-05-21 10:30:49 +0530

    save printnumbers

---
+++ loops/PrintNumbers.java
@@ -0,0 +1,9 @@
+package loops;
+public class PrintNumbers {
+    public static void main(String[]args) {
+        int a;
+        for(a=1; a<11; a++) {
+            System.out.println(a);
+        }
+    }
+}
