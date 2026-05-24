d99bfaa0d88c82a7993088b0e0377820db1ae1f9
Ritika-23-2007 <ritika.agrwl.2007@gmail.com>
2026-05-24 20:47:17 +0530

    reverse of a number

--- loops/ReverseNumber.java
+++ loops/ReverseNumber.java
@@ -4,8 +4,21 @@ public class ReverseNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        //Taking input from the user.
+        //Taking input from user.
         System.out.println("Enter an integer : ");
-        long number = cs.
+        long number = sc.nextLong();
+        long value = Math.abs(number);
+
+        //Reversing input number.
+        int reverse = 0;
+        while (value != 0) {
+            reverse *= 10;
+            reverse += value % 10;
+            value /= 10;
+        }
+
+        //Print output & close scanner class.
+        System.out.println("The reverse of " +number+ " is : " +reverse);
+        sc.close();
     }
 }
