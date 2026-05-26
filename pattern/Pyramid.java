70b00634d0d0367baaf9291601a23a0cac62641b
Ritika-23-2007 <ritika.agrwl.2007@gmail.com>
2026-05-26 18:20:48 +0530

    save pyramid

---
+++ pattern/Pyramid.java
@@ -0,0 +1,25 @@
+package pattern;
+import java.util.Scanner;
+public class Pyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        if (rows <= 0) {
+            System.out.println("Please enter a positive integer for rows.");
+            sc.close();
+            return;
+        }
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows; j++) {
+                System.out.print((j <= rows - i) ? "  " : "* ");
+            }
+            for (int j = 1; j < i; j++) {
+                System.out.print("* ");
+            }
+            
+            System.out.println();
+        }
+        sc.close();
+    }
+}
