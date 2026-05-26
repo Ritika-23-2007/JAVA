7e685cb13f041dbcb1229429c95b848c098a5579
Ritika-23-2007 <ritika.agrwl.2007@gmail.com>
2026-05-26 17:38:31 +0530

    save odd number triangle

---
+++ pattern/OddTriangle.java
@@ -0,0 +1,24 @@
+package pattern;
+import java.util.Scanner;
+public class OddTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+
+        if (rows <= 0) {
+            System.out.println("Please enter a positive integer for rows.");
+            sc.close();
+            return;
+        }
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print((2 * j - 1) + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
