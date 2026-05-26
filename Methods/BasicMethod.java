b2604bb65bbe52c00de57aa92e778fe4f6a3823f
Ritika-23-2007 <ritika.agrwl.2007@gmail.com>
2026-05-26 21:03:26 +0530

    save basic method

---
+++ Methods/BasicMethod.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class BasicMethod {
+        public static void main(String[] args) {
+        myMethod();
+    }
+    public static void myMethod() {
+        System.out.println("Hello, World!");
+    }
+
+}
