da05ea115917bb8c5a3b7873e6a0495f21214622
Ritika-23-2007 <ritika.agrwl.2007@gmail.com>
2026-05-27 21:01:23 +0530

    maximum element of array

--- src/arrays/MinimumElement.java
+++ src/arrays/MinimumElement.java
@@ -6,7 +6,7 @@ public class MinimumElement {}
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the sizej of array: ");
+        System.out.print("Enter the size of array: ");
         int size = sc.nextInt();
 
         if (size <= 0) {
