de8467caad0e55f92a3817a8871f6ae2b21ea28c
Ritika-23-2007 <ritika.agrwl.2007@gmail.com>
2026-05-27 21:02:50 +0530

    minor change

--- src/arrays/ProductOfElements.java
+++ src/arrays/ProductOfElements.java
@@ -4,7 +4,7 @@ public class ProductOfElements {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the size of the array: ");
+        System.out.println("Enter the size of the array: ");
         int size = sc.nextInt();
 
         if (size <= 0) {
