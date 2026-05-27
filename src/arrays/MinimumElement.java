d5a54630ca384d9ef1afa9f4ab9cbce39ea29ac4
Ritika-23-2007 <ritika.agrwl.2007@gmail.com>
2026-05-27 21:01:52 +0530

    maximum element of array

--- src/arrays/MinimumElement.java
+++ src/arrays/MinimumElement.java
@@ -2,7 +2,7 @@ package arrays;
 
 import java.util.Scanner;
 
-public class MinimumElement {}
+public class MinimumElement {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
