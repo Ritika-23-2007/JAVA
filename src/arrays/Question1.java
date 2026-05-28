aaacc2f2f466b2ebb2935fbb123380b391525d6f
Ritika-23-2007 <ritika.agrwl.2007@gmail.com>
2026-05-28 09:28:03 +0530

    add a question

---
+++ src/arrays/Question1.java
@@ -0,0 +1,48 @@
+//Multiply odd indexed elements by 2 and add 10 to even indexed elements in array and print the new array.
+package arrays;
+import java.util.Scanner;
+public class Question1 {
+
+    public static void printArray(int[] array) {
+        System.out.println("The array is:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static int[] modifyArray(int[] array) {
+        int[] modifiedArray = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0) { // Even indexed element
+                modifiedArray[i] = array[i] + 10;
+            } else { // Odd indexed element
+                modifiedArray[i] = array[i] * 2;
+            }
+        }
+        return modifiedArray;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+
+        if (size <= 0) {
+            System.out.println("Size must be a positive integer.");
+            sc.close();
+            return;
+        }
+        int[] array = new int[size];
+        System.out.println("Enter " + size + " integers:");
+        for (int i = 0; i < size; i++) {
+            array[i] = sc.nextInt();
+        }
+        System.out.println("Original array:");
+        printArray(array);
+        int[] modifiedArray = modifyArray(array);
+        System.out.println("Modified array:");
+        printArray(modifiedArray);
+        sc.close();
+    }
+}
