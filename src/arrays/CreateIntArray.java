06d3ae040d24cc2a6b6c827d6fc68c0d32e8ce3b
Ritika-23-2007 <ritika.agrwl.2007@gmail.com>
2026-05-28 12:40:55 +0530

    create fixed sized array

--- src/arrays/CreateIntArray.java
+++ src/arrays/CreateIntArray.java
@@ -16,4 +16,17 @@ public class CreateIntArray {
         }
         return array1; // Return the created array
     }
+
+    public static int[] createFixSizedArray(int size, Scanner sc) {
+        if (size <= 0) {
+            System.out.println("Array size must be greater than 0.");
+            return null; // Return null to indicate an invalid array size
+        }
+        int[] array1 = new int[size];
+        System.out.println("Enter the " + size + " integers of the array:");
+        for (int i = 0; i < size; i++) {
+            array1[i] = sc.nextInt();
+        }
+        return array1; // Return the created array
+    }
 }
