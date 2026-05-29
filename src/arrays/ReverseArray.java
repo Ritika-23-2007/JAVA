f74c588b295f73b9ed183ef95459853fe087cc2b
Ritika-23-2007 <ritika.agrwl.2007@gmail.com>
2026-05-29 11:04:30 +0530

    Reverse of an array

---
+++ src/arrays/ReverseArray.java
@@ -0,0 +1,21 @@
+package arrays;
+import java.util.Scanner;
+public class ReverseArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] originalArray = CreateIntArray.createIntArray(sc);
+        int[] reversedArray = new int[originalArray.length];
+        for (int i = 0; i < originalArray.length; i++) {
+            reversedArray[i] = originalArray[originalArray.length - 1 - i];
+        }
+        System.out.println("Original Array:");
+        for (int num : originalArray) {
+            System.out.print(num + " ");
+        }
+        System.out.println("\nReversed Array:");
+        for (int num : reversedArray) {
+            System.out.print(num + " ");
+        }
+        sc.close();
+    }
+}
