df77e7cc651c70050fd9ca0f53565621dec24c02
Ritika-23-2007 <ritika.agrwl.2007@gmail.com>
2026-05-30 20:03:40 +0530

    merge two sorted array into a new sorted array

---
+++ src/arrays/MergeSort2Arrays.java
@@ -0,0 +1,26 @@
+package arrays;
+//given two sorted arrays merge them and sort them into one sorted array
+public class MergeSort2Arrays {
+    public static int[] mergeSortedArrays(int[] arr1, int[] arr2) {
+        int[] mergedArray = new int[arr1.length + arr2.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                mergedArray[k++] = arr1[i++];
+            } else {
+                mergedArray[k++] = arr2[j++];
+            }
+        }
+
+        while (i < arr1.length) {
+            mergedArray[k++] = arr1[i++];
+        }
+
+        while (j < arr2.length) {
+            mergedArray[k++] = arr2[j++];
+        }
+
+        return mergedArray;
+    }
+}
