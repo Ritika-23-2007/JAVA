8b571f1e0d4f8fe76812233f962f49690ed13ef5
Ritika-23-2007 <ritika.agrwl.2007@gmail.com>
2026-05-30 20:01:08 +0530

    find second maximum element in arraay

--- src/arrays/SecondMaximum.java
+++ src/arrays/SecondMaximum.java
@@ -1,5 +1,19 @@
 package arrays;
 
 public class SecondMaximum {
-    
+    public static int secondMax(int[] arr)  {
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > max) {
+                secondMax = max;
+                max = num;
+            } else if (num > secondMax && num != max) {
+                secondMax = num;
+            }
+        }
+
+        return secondMax;
+    } 
 }
