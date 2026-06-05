0d0f71b3dc6794a09b3f56e7e41b6b44f3829196
Ritika-23-2007 <ritika.agrwl.2007@gmail.com>
2026-06-05 15:18:29 +0530

    check if array is sorted

---
+++ src/arrays/CheckSortedArray.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class CheckSortedArray {
+    public static boolean isSorted(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
