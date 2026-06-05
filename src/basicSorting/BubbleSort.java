d5d3b1547f2cf68a537e6e5d912599cdb3bc7f44
Ritika-23-2007 <ritika.agrwl.2007@gmail.com>
2026-06-05 19:52:44 +0530

    add definition in comment

--- src/basicSorting/BubbleSort.java
+++ src/basicSorting/BubbleSort.java
@@ -2,10 +2,18 @@ package basicSorting;
 import java.util.Scanner;
 import java.util.Arrays;
 import arrays.CreateIntArray;
+
+//bubble sort is a simple sorting algorithm 
+// that repeatedly steps through the list, 
+// compares adjacent elements and swaps them if they are in the wrong order. 
+// The pass through the list is repeated until the list is sorted.
 public class BubbleSort {
     public static void bubbleSort(int[] arr) {
         int length = arr.length;
         for (int i = 0; i < length - 1; i++) {
+            if(isSorted(arr)) {
+                return;
+            }
             for (int j = 0; j < length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     // Swap arr[j] and arr[j+1]
