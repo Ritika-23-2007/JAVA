3db41f8624e729a52ac915b43f34ff4cc87b2fdd
Ritika-23-2007 <ritika.agrwl.2007@gmail.com>
2026-07-28 16:13:48 +0530

    understand bubble sort by different approach to optimizr time

--- src/basicSorting/BubbleSort.java
+++ src/basicSorting/BubbleSort.java
@@ -1,14 +1,19 @@
 package basicSorting;
-import java.util.Scanner;
-import java.util.Arrays;
-import arrays.CreateIntArray;
 
 //bubble sort is a simple sorting algorithm 
 // that repeatedly steps through the list, 
 // compares adjacent elements and swaps them if they are in the wrong order. 
 // The pass through the list is repeated until the list is sorted.
 public class BubbleSort {
-    public static void bubbleSort(int[] arr) {
+
+    //approach one 
+    //uses bubble sort and before every iteration checks if the array is already sorted or not
+    //if the array is already sorted then it will return and not perform any further iterations
+    //this will reduce the time complexity of the algorithm in best case scenario to O(n)
+    //and in all cases except the worst case scenario, the time will reduce.
+    //but in worst case scenario the time complexity will worsen because of the extra check for sorted array which will take O(n) time
+    
+    public static void bubbleSort1(int[] arr) {
         int length = arr.length;
         for (int i = 0; i < length - 1; i++) {
             if(isSorted(arr)) {
@@ -25,10 +30,57 @@ public class BubbleSort {
         }
     }
 
-    public static void printArray(int[] arr) {
-        System.out.println(Arrays.toString(arr));
+    //approach two
+    //uses bubble sort and before starting algo, it checks if the array is already sorted or not
+    //if the array is already sorted then it will return and not perform any further iterations
+    //this will reduce the time complexity of the algorithm in best case scenario to O(n)
+    //and in rest of all time complexity will remain same as O(n^2) .
+
+    public static void bubbleSort2(int[] arr) {
+        int length = arr.length;
+        if(isSorted(arr)) {
+            return;
+        }
+        for (int i = 0; i < length - 1; i++) {
+             for (int j = 0; j < length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
     }
 
+    //approach three
+    //uses bubble sort to sort the array and uses a int swapCount variable to keep track of the number of swaps made in each iteration
+    //if the swapCount is zero then it means that the array is already sorted and we can return from the function
+    //this will reduce the time complexity of the algorithm in best case scenario to O(n)
+    //and in rest of all time complexity will remain same as O(n^2) 
+    //but it will be better than approach one and two 
+    //because it will not check for sorted array in every iteration but only when the swapCount is zero.
+
+    public static void bubbleSort3(int[] arr) {
+        int length = arr.length;
+        int swapCount = 0;
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapCount++;
+                }
+            }
+            if(swapCount == 0) {
+                return;
+            }
+            else swapCount = 0;
+        }
+    }
+     
     public static boolean isSorted(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i] > arr[i + 1]) {
@@ -37,13 +89,5 @@ public class BubbleSort {
         }
         return true;
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] arr = CreateIntArray.createIntArray(sc);
-        System.out.println("Before sorting:");
-        printArray(arr);
-        bubbleSort(arr);
-        System.out.println("After sorting:");
-        printArray(arr);
-    }
+    
 }
