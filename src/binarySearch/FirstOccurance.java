5d080b122bcee801dba95e31d2cfaca4034c69c1
Ritika-23-2007 <ritika.agrwl.2007@gmail.com>
2026-08-03 19:07:49 +0530

    binary search to find first index of duplicate index

---
+++ src/binarySearch/FirstOccurance.java
@@ -0,0 +1,24 @@
+package binarySearch;
+
+public class FirstOccurance {
+    public static int firstOccurence(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                result = mid; // Update result and continue searching in the left half
+                right = mid - 1;
+            } else if (arr[mid] < target) {
+                left = mid + 1; // Search in the right half
+            } else {
+                right = mid - 1; // Search in the left half
+            }
+        }
+
+        return result; // Return the index of the first occurrence or -1 if not found
+    }
+}
