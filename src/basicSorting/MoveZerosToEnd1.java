9113a28d49fba61abad6fc2892a3fafc83eddb2e
Ritika-23-2007 <ritika.agrwl.2007@gmail.com>
2026-09-04 15:31:51 +0530

    used different approach to solve this problem

--- src/basicSorting/MoveZerosToEnd1.java
+++ src/basicSorting/MoveZerosToEnd1.java
@@ -16,7 +16,7 @@ public class MoveZerosToEnd1 {
         }
     }
 
-    // using two pointer (opposite direction) approach
+    // using two pointer (same direction) approach
     public static void moveZerosToEndTwoPointer(int[] arr) {
         int n = arr.length;
         int nonZeroIndex = 0;
@@ -31,11 +31,17 @@ public class MoveZerosToEnd1 {
         }
     }
 
-    // using two pointer (same direction) approach
+    // using two pointer (opp direction) approach- 
+    //it can be solved using two pointer approach in opposite direction as well, 
+    // where one pointer starts from the beginning and the other from the end of the array. 
+    // The first pointer moves forward until it finds a zero, and the second pointer moves backward until it finds a non-zero element. 
+    // When both pointers find their respective elements, they swap them. 
+    // This process continues until the two pointers meet.
+    //but it will not maintain the order of non-zero elements in the array.k
+    //and if we maintain order either it will take O(n^2) time complexity or O(n) space complexity.
 
-    //using summing technique and then overwriting the array with non-zero elements and then filling the rest of the array with zeros
-    public static void main(String[] args) {
+    //using summing technique and then overwriting the array with non-zero elements 
+    // and then filling the rest of the array with zeros
+    //but it applies only when the array contains only one element other than zero and that element is known in advance.
 
-    }
-}
 
