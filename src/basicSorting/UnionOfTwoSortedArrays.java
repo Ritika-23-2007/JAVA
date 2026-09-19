6366f2ce69193338596577772120ed80b9ef44ba
Ritika-23-2007 <ritika.agrwl.2007@gmail.com>
2026-09-19 14:15:50 +0530

    find union of two alread sorted arrays

---
+++ src/basicSorting/UnionOfTwoSortedArrays.java
@@ -0,0 +1,45 @@
+package basicSorting;
+
+import java.util.ArrayList;
+
+public class UnionOfTwoSortedArrays {
+    //given two sorted arrays, where each array may contain duplicate elements, return the union of the two arrays in a sorted order.
+    //dont write any duplicate elements in the output array.
+    public static ArrayList<Integer> union(int[] arr1, int[] arr2) {
+        ArrayList<Integer> result = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                if (result.isEmpty() || result.get(result.size() - 1) != arr1[i]) {
+                    result.add(arr1[i]);
+                }
+                i++;
+            } else if (arr1[i] > arr2[j]) {
+                if (result.isEmpty() || result.get(result.size() - 1) != arr2[j]) {
+                    result.add(arr2[j]);
+                }
+                j++;
+            } else {
+                if (result.isEmpty() || result.get(result.size() - 1) != arr1[i]) {
+                    result.add(arr1[i]);
+                }
+                i++;
+                j++;
+            }
+        }
+        while (i < arr1.length) {
+            if (result.isEmpty() || result.get(result.size() - 1) != arr1[i]) {
+                result.add(arr1[i]);
+            }
+            i++;
+        }
+        while (j < arr2.length) {
+            if (result.isEmpty() || result.get(result.size() - 1) != arr2[j]) {
+                result.add(arr2[j]);
+            }
+            j++;
+        }
+        return result;
+    }
+
+}
